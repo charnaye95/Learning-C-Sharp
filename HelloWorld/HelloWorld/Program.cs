68b6703763043a81405dcf9a81fdf4c123f8d1eb
charnaye95 <charnayegrier@gmail.com>
2022-12-29 17:17:42 -0500

    finished module 1 of CodeAcademy Course

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Charnaye is learning this thing! 1 st@p at a time...");
 var testVar = "Naye Naye";
 Console.WriteLine(testVar);
+var input = Console.ReadLine();
+Console.WriteLine(input);
